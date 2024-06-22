50f5706337414cc787a65c2dc225d9aa80b48289
Dhruvsavani47 <154688493+Dhruvsavani47@users.noreply.github.com>
2024-06-22 17:38:57 +0530

    ready final project

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwR3r-dEdVNtTimv1155DiTJ6Y5lfLhII",
+  authDomain: "mern-estate-a3151.firebaseapp.com",
+  projectId: "mern-estate-a3151",
+  storageBucket: "mern-estate-a3151.appspot.com",
+  messagingSenderId: "737620094714",
+  appId: "1:737620094714:web:a4bc1e1c32bb31f1232343"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
